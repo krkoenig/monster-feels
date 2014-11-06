386c29d3779e2b5d27e5c7cf6e0d620484669d12
Michael Sarfati <macehelix@gmail.com>
2014-11-05 19:41:30 -0800

    Added a start screen and basic menus.
    Press a button to go to that level; also you can press escape to open or
    close the menu in a level. (Also I moved the cameras a bit)

---
+++ MonsterFeelings/Assets/Menu_GUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_GUI : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+		GUI.Box(new Rect(10,10,220,100), "This is the Start Menu");
+		//(x pos, y pos, x width, y width)
+
+		// Make the first button. If it is pressed, the first level will be loaded
+		if(GUI.Button(new Rect(20,40,200,20), "One Character")) {
+			Application.LoadLevel("singleCharTest");
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,200,20), "Two Characters")) {
+			Application.LoadLevel("twoCharTest");
+		}
+	}
+}
+
+//To add more levels, just follow the format
+//To change the level name, just change what's in the quotes in Application.LoadLevel("")
---
+++ MonsterFeelings/Assets/Menu_GUI_2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu_GUI_2 : MonoBehaviour {
+
+	bool display_gui = false;
+
+	void OnGUI () {
+		// Make a background box
+		if (display_gui == true)
+		{
+
+			GUI.Box(new Rect(10,10,100,90), "Menu");
+			
+			// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+			if(GUI.Button(new Rect(20,40,80,20), "Level 1")) {
+				Application.LoadLevel(1);
+			}
+			
+			// Make the second button.
+			if(GUI.Button(new Rect(20,70,80,20), "Start Menu")) {
+				Application.LoadLevel(0);
+			}
+		}
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown ("escape"))
+		{
+			display_gui = !display_gui;
+		}
+
+	}
+}
